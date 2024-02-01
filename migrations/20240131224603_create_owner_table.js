52eafc0d23033dd1f1c6d6ea79af05d416ab6c6c
tomas-tank3ngine <tomasmartinez1424@gmail.com>
2024-01-31 19:20:22 -0500

    create migrate tables

---
+++ migrations/20240131224603_create_owner_table.js
@@ -0,0 +1,26 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.createTable("owners", (table) => {
+      table.increments("id").primary();
+      table.string("username").notNullable();
+      table.string("password").notNullable();
+      table.string("email").notNullable();
+      table.timestamp("updated_at").defaultTo(knex.fn.now());
+  
+      table.timestamp("created_at").defaultTo(knex.fn.now());
+      table
+        .timestamp("updated_at")
+        .defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"));
+    });
+  };
+  
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  exports.down = function (knex) {
+      return knex.schema.dropTable('owners');
+  };
---
+++ migrations/20240131224725_create_room_table.js
@@ -0,0 +1,38 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.createTable("rooms", (table) => {
+      table.increments("id").primary();
+      table.string("name").notNullable();
+      table.string("description").notNullable();
+      table.string("theme").notNullable();
+      table.string("address").notNullable();
+      table.string("cost").notNullable();
+      table.int("group-size").notNullable();
+      table.int("duration").notNullable();
+      table.string("difficulty").notNullable();
+      table.float("success-rate").notNullable();
+      table.float("overall-rating").notNullable();
+      table.float("atmosphere-rating").notNullable();
+      table.float("puzzle-fairness-rating").notNullable();
+      table.float("tech-rating").notNullable();
+      table.float("storyline-rating").notNullable();
+      table.float("staff-rating").notNullable();
+
+      table.timestamp("updated_at").defaultTo(knex.fn.now());  
+      table.timestamp("created_at").defaultTo(knex.fn.now());
+      table
+        .timestamp("updated_at")
+        .defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"));
+    });
+  };
+  
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  exports.down = function (knex) {
+      return knex.schema.dropTable('rooms');
+  };
